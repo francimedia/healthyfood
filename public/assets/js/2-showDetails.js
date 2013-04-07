f4814de1403fc500085ec093e9013e8721ab363d
Wilkins <wilkins@noisenewyork.com>
2013-04-07 15:23:57 -0400

    Use only one price when submitting a price entry for an item

--- public/assets/js/2-showDetails.js
+++ public/assets/js/2-showDetails.js
@@ -14,16 +14,32 @@ App.Details = (function () {
 })();
 
 $$(function () {
-    // Shortcut
     var data = App.Details.data;
+
+    var $price = $$('.price');
+    // Clear prices for fields onload
+    $price.val('');
+
+
     $$('#subpage').on('load', function (e) {
 
     });
 
     $$('#submit-price').on('tap', function () {
         // Add price for item
-        data.price = $$('.price').val();
-        console.log(data);
+        data.price = $price.val();
         Lungo.Service.post(App.config.priceURL, data, App.Details.parseResponse, "json")
     });
+
+    // Use only one price when sending
+    $price.on('change', function(){
+        if($$(this).hasClass('per-pound')){
+            $$('.single').val('');
+        }
+        if($$(this).hasClass('single')){
+            $$('.per-pound').val('');
+        }
+    })
+
+
 });
